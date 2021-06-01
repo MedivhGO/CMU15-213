c61a5b1e4f86c526f4c5d09125ba9c2e5f5e5806
MedivhGo <lijingqi93@gmail.com>
2021-06-01 22:49:30 +0800

    solved howmanybits

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -286,7 +286,25 @@ int logicalNeg(int x) {
  *  Rating: 4
  */
 int howManyBits(int x) {
-  return 0;
+  /*
+  最少使用多少位，使用二进制补码进行表示x的值。
+  */
+  int b16,b8,b4,b2,b1,b0;
+  int sign = x >> 31;
+  x = (sign & ~x) | (~sign & x);
+  
+  b16 = !!(x >> 16) << 4;
+  x = x >> b16;
+  b8 = !!(x>>8) << 3;
+  x = x >> b8;
+  b4 = !!(x>>4) << 2;
+  x = x>>b4;
+  b2 = !!(x >> 2) << 1;
+  x = x>>b2;
+  b1 = !!(x>>1);
+  x = x >> b1;
+  b0 = x;
+  return b16+b8+b4+b2+b1+b0+1;
 }
 //float
 /* 
